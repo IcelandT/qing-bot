db52e22a8fec39e6220d72e95188b024b14d9d48
Iceland <1185330343@qq.com>
2023-05-15 16:39:48 +0800

    pq

--- flask/parsel_message.py
+++ flask/parsel_message.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from tools import color_image, tips
+from tools import database_operations
 import os
 
 
@@ -12,16 +13,19 @@ def parsel(message, qq_number, group_number=None):
     :return:
     """
     # 判断用户输入
-    if "色图" in message:
+    if "色图" and "涩涩" in message:
         color_image.send_color_image(group_number)
     if "更新图库" in message:
         if qq_number == 1185330343:
-            tips.normal_tip(group_number, "Ice收到主人的命令: 更新图库")
+            tips.normal_tip(group_number, "Ice收到主人下达的命令: 更新图库")
+
             this_path = os.getcwd()
             path = this_path + "\\tools\\image_spider.py"
             os.system(f"start cmd.exe /c python {path} -g {group_number}")
         else:
             tips.normal_tip(group_number, "对不起, 你没有权限这样做", qq_number)
+    if "图库大小" in message:
+        database_operations.QueryTableSize("pixiv_image_id").query_table_size(group_number)
 
 
 
